f42497927528289cc59c847dbed0f34a6c2ab0d9
MPurusothamNaidu-Wal <purusotham.m@westagilelabs.com>
2022-05-19 18:41:46 +0530

    commited successfully

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,6 @@ var express = require('express');
 
 var cors = require('cors');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/book');
@@ -18,24 +15,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use(
-  session({
-    secret: 'session_secret_key',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-    },
-  })
-);
-
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
 app.use('/categories', categoryRouter);
--- backend/dbsq.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const db = new Sequelize('product', 'root', 'umaushan', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = db;
